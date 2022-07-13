bb7ab0409cfd2f4ca267274b6ba35fefc001d29f
Michael <misha060814@gmail.com>
2022-07-13 22:22:22 +0300

    HomeWork6

---
+++ HomeWork6/Task41/Program.cs
@@ -0,0 +1,34 @@
+﻿int userLength = InputLengthUserArray("Введите длину массива ") ;
+int [] arrayNumbers =  new int [userLength];
+UserInputNumbers(arrayNumbers);
+PrintArrayGreaterZero (arrayNumbers);
+
+void UserInputNumbers (int [] arrayNumbers)
+{
+    for (int i = 0 ; i < arrayNumbers.Length ; i++)
+    {
+        Console.WriteLine($"Введите " + (i+1) +" номер: ");
+        arrayNumbers [i] = Convert.ToInt16(Console.ReadLine()); 
+    }
+
+}
+void PrintArrayGreaterZero (int [] arrayNumbers)
+{
+    int numbersGreaterZero = 0;
+    Console.Write("Элементы больше нуля: (");
+    for (int i = 0; i < arrayNumbers.Length; i++)
+    {
+        if (arrayNumbers[i] > 0)
+        {
+            numbersGreaterZero++;
+
+            Console.Write(" " + arrayNumbers[i] + " " );
+        }
+    }
+    Console.Write(") Количество элементов больше нуля: " + numbersGreaterZero );
+}
+int InputLengthUserArray ( string lng)
+{
+    Console.Write (lng);
+    return Convert.ToInt16(Console.ReadLine());
+}
---
+++ HomeWork6/Task43/Program.cs
@@ -0,0 +1,13 @@
+﻿double b1 = UserInputNumbers("Введите значение для b1 ");
+double k1 = UserInputNumbers("Введите значение для k1 ");
+double b2 = UserInputNumbers("Введите значение для b2 ");
+double k2 = UserInputNumbers("Введите значение для k2 ");
+double x = (b2 - b1)/(k1 - k2);
+double y = k1*(b2-b1)/(k1-k2)+b1;
+Console.WriteLine("Координаты точки пересечения: [" + x + ";" + y + "]");
+
+double UserInputNumbers (string numbers)
+{
+    Console.Write(numbers);
+    return Convert.ToDouble(Console.ReadLine());
+} 
